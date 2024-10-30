c1aa02c344c741599311545dd3eab2b522605911
Juan Torres <juanestebanrendontorres2005@gmail.com>
2024-10-29 22:20:36 -0500

    Implement LanguagePicker using in18

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
@@ -12,7 +13,8 @@ export default defineConfig({
   alias: {
     styles: './src/styles'
   },
-
+  output: 'server',
+  adapter: vercel(),
   integrations: [react(), tailwind(), sitemap()],
 
   i18n: {
