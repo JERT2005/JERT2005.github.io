6c429028354254527e5fa0310b358a5e182a9b8b
Juan Torres <juanestebanrendontorres2005@gmail.com>
2024-10-29 21:47:03 -0500

    Config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel'; // Importa el adaptador de Vercel
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,14 +12,14 @@ export default defineConfig({
   alias: {
     styles: './src/styles'
   },
-  output:'server',
+  output: 'server',
   integrations: [react(), tailwind(), sitemap()],
+  adapter: vercel(), // Configura el adaptador de Vercel
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
     routing: {
       prefixDefaultLocale: false
-  }
-  
+    }
   }
 });
